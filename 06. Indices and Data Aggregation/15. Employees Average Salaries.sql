d52c9e58866e250bd12ca2848333d98468369e7c
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-01-18 00:03:34 +0200

    Add files via upload

---
+++ 06. Indices and Data Aggregation/15. Employees Average Salaries.sql
@@ -0,0 +1,16 @@
+SELECT * INTO [EmployeesNewTable]
+FROM [Employees]
+WHERE [Salary] > 30000;
+
+DELETE FROM [EmployeesNewTable]
+WHERE [ManagerID] = 42;
+
+UPDATE [EmployeesNewTable]
+SET [Salary] = [Salary] + 5000
+WHERE [DepartmentID] = 1;
+
+SELECT 
+	[DepartmentID],
+	AVG([Salary]) AS [AverageSalary]
+FROM [EmployeesNewTable]
+GROUP BY [DepartmentID];
