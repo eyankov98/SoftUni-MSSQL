865d1fceb2fd8d198197c28cc8d45b9315f3681c
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-01-18 00:04:35 +0200

    Add files via upload

---
+++ 06. Indices and Data Aggregation/19. Salary Challenge.sql
@@ -0,0 +1,13 @@
+SELECT TOP(10)
+	[FirstName],
+	[LastName],
+	[DepartmentID]
+FROM [Employees] AS [e1]
+WHERE [Salary] > (	
+					SELECT
+						AVG([Salary]) AS [AverageSalary]
+					FROM [Employees] AS [e2]
+					WHERE [e1].[DepartmentID] = [e2].[DepartmentID]
+					GROUP BY [DepartmentID]
+				)
+ORDER BY [DepartmentID];
