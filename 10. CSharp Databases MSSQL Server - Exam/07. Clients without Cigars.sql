e5b30f20891d9d65ccffdeccf1982b081778e033
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-01-28 23:28:36 +0200

    Add files via upload

---
+++ 10. CSharp Databases MSSQL Server - Exam/07. Clients without Cigars.sql
@@ -0,0 +1,8 @@
+SELECT
+	[c].[Id]
+	,CONCAT_WS(' ', [c].[FirstName], [c].[LastName]) AS [ClientName]
+	,[c].[Email]
+FROM [Clients] AS [c]
+	LEFT JOIN [ClientsCigars] AS [cc] ON [cc].[ClientId] = [c].[Id]
+WHERE [cc].[CigarId] IS NULL
+ORDER BY [ClientName] ASC
