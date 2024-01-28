50415e1821e1c409f59f021a51a4ee38a637438c
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-01-28 23:28:53 +0200

    Add files via upload

---
+++ 10. CSharp Databases MSSQL Server - Exam/08. First 5 Cigars.sql
@@ -0,0 +1,8 @@
+SELECT TOP(5)
+	[c].[CigarName]
+	,[c].[PriceForSingleCigar]
+	,[c].[ImageURL]
+FROM [Cigars] AS [c]
+	INNER JOIN [Sizes] AS [s] ON [s].[Id] = [c].[SizeId]
+WHERE [s].[Length] >= 12 AND ([c].[CigarName] LIKE '%ci%' OR [c].[PriceForSingleCigar] > 50.00) AND [s].[RingRange] > 2.55
+ORDER BY [c].[CigarName] ASC, [c].[PriceForSingleCigar] DESC
