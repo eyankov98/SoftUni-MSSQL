6477c9796011ac002e3983e5afc87e0328b8cf6b
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-02-03 21:28:53 +0200

    Delete 12. CSharp Databases MSSQL Server - Exam/06. Sites with Their Location and Category.sql

--- 12. CSharp Databases MSSQL Server - Exam/06. Sites with Their Location and Category.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-	[s].[Name] AS [Site]
-	,[l].[Name] AS [Location]
-	,[s].[Establishment]
-	,[c].[Name] AS [Category]
-FROM [Sites] AS [s]
-	INNER JOIN [Locations] AS [l] ON [l].[Id] = [s].[LocationId]
-	INNER JOIN [Categories] AS [c] ON [c].[Id] = [s].[CategoryId]
-ORDER BY [c].[Name] DESC, [l].[Name] ASC, [s].[Name] ASC
