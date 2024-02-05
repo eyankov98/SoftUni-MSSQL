5aefd2b11870cbe81339dbb3fe35830bdf2359b4
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-02-05 23:11:50 +0200

    Add files via upload

---
+++ 13. CSharp Databases MSSQL Server - Exam/07. Creators without Boardgames.sql
@@ -0,0 +1,8 @@
+SELECT
+	[c].[Id]
+	,CONCAT_WS(' ', [c].[FirstName], [c].[LastName]) AS [CreatorName]
+	,[c].[Email]
+FROM [Creators] AS [c]
+	LEFT JOIN [CreatorsBoardgames] AS [cb] ON [cb].[CreatorId] = [c].[Id]
+WHERE [cb].[BoardgameId] IS NULL
+ORDER BY [CreatorName] ASC
