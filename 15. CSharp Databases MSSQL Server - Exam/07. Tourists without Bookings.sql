f853e9a0a10bc32a4df96d3bebf0b0413d3afe4b
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-02-07 23:39:41 +0200

    Add files via upload

---
+++ 15. CSharp Databases MSSQL Server - Exam/07. Tourists without Bookings.sql
@@ -0,0 +1,8 @@
+SELECT 
+	[t].[Id]
+	,[t].[Name]
+	,[t].[PhoneNumber]
+FROM [Tourists] AS [t]
+	LEFT JOIN [Bookings] AS [b] ON [b].[TouristId] = [t].[Id]
+WHERE [b].[TouristId] IS NULL
+ORDER BY [t].[Name] ASC
