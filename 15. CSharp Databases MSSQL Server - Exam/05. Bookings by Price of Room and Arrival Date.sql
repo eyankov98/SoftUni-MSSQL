129446830b66ee812c4edd0ab349ceb452ee203e
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-02-07 23:39:13 +0200

    Add files via upload

---
+++ 15. CSharp Databases MSSQL Server - Exam/05. Bookings by Price of Room and Arrival Date.sql
@@ -0,0 +1,7 @@
+SELECT
+	FORMAT([ArrivalDate], 'yyyy-MM-dd') AS [ArrivalDate]
+	,[AdultsCount]
+	,[ChildrenCount]
+FROM [Bookings] AS [b]
+	INNER JOIN [Rooms] AS [r] ON [r].[Id] = [b].[RoomId]
+ORDER BY [r].[Price] DESC, [ArrivalDate] ASC
